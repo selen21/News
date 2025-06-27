4b963748c623fef2aabf778774177809a2bc1bcb
selen21 <21selenkorkmaz@gmail.com>
2025-06-27 12:55:54 +0300

    Güncelleme: Görünüm iyileştirildi. Update: Appearance improved.

--- src/app/news/news-list/news-list.component.ts
+++ src/app/news/news-list/news-list.component.ts
@@ -1,4 +1,76 @@
 import { Component, OnInit } from '@angular/core';
+import { debounceTime, distinctUntilChanged, Subject, switchMap } from 'rxjs';
+import { NewsService } from 'src/app/shared/news.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-news-list',
+  templateUrl: './news-list.component.html',
+  styleUrls: ['./news-list.component.scss']
+})
+export class NewsListComponent implements OnInit {
+  articles: any[] = [];
+  sliderArticles: any[] = [];
+  searchQuery = '';
+  activeCategory = '';
+  private searchSubject = new Subject<string>();
+
+  constructor(private newsService: NewsService, private router: Router) { }
+
+  ngOnInit(): void {
+    this.loadTopHeadlines();
+
+    this.searchSubject.pipe(
+      debounceTime(300),
+      distinctUntilChanged(),
+      switchMap(query => this.newsService.searchArticles(query))
+    ).subscribe((data: any) => {
+      this.articles = data.articles;
+      this.updateSliderArticles();
+    });
+  }
+
+  loadTopHeadlines() {
+    this.newsService.getTopHeadlines().subscribe((data: any) => {
+      this.articles = data.articles;
+      this.updateSliderArticles();
+    });
+  }
+
+  updateSliderArticles() {
+    this.sliderArticles = this.articles.slice(0, 10); // İlk 10 haberi slider yap
+  }
+
+  onSearchChange(query: string) {
+    this.activeCategory = '';
+    if (query.trim().length > 2) {
+      this.searchSubject.next(query);
+    } else if (!query.trim()) {
+      this.loadTopHeadlines();
+    }
+  }
+
+  filterByCategory(category: string) {
+    this.activeCategory = category;
+    this.searchQuery = '';
+    this.newsService.getByCategory(category).subscribe((data: any) => {
+      this.articles = data.articles;
+      this.updateSliderArticles();
+    });
+  }
+
+  goToDetail(article: any) {
+    this.newsService.selectedArticle = article;
+    // URL encode et
+    const encodedUrl = encodeURIComponent(article.url);
+    this.router.navigate(['/news', encodedUrl]);
+  }
+}
+
+
+
+
+/*import { Component, OnInit } from '@angular/core';
 import { NewsService } from 'src/app/shared/news.service';
 import { Router } from '@angular/router';
 import { Subject } from 'rxjs';
@@ -54,7 +126,7 @@ export class NewsListComponent implements OnInit {
       this.articles = data.articles;
     });
   }
-}
+}*/
 
 
 
